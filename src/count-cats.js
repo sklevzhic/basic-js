3f77c1920973c4827c224f5f34cf9aedbfb61ee1
Alexander Klevzhits <sklevzhic@gmail.com>
2020-02-16 19:27:58 +0300

    init: start basic-js task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,12 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let number = 0;
+  for (i = 0; i < matrix.length; i++) {
+    for (j = 0; j < matrix[i].length; j++)  {
+      if (matrix[i][j] == "^^") {
+        number += 1;
+      }
+    }
+  }
+
+  return number;
 };
